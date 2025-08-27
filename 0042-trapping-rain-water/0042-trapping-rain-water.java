aad5fb583e4297113b18d43415538f59d8aafcfc
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2025-08-27 23:00:01 +0530

    Update 0042-trapping-rain-water.java

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,5 +1,9 @@
 class Solution {
     public int trap(int[] height) {
+        /*Two ptr approach
+        T(n) = O(n)
+        S(n) = O(1)*/
+        //logic l < r => l++ res+=leftMax-height[l] simly for r
         int l = 0, r = height.length - 1;
         int leftMax = height[l], rightMax = height[r];
         int res = 0;
