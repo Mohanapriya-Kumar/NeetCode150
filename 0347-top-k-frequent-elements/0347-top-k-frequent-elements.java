697a0eafbf58fbaffb9fa7370f2e9b4a8f37aeca
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2025-08-27 20:09:48 +0530

    Time: 9 ms (97.51%), Space: 49.9 MB (5.04%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        /*Storing map count for num:freq mapping, Array freq with each index value: AL of elements whose count = i(index of freq Array)
+        T(n) = O(n);
+        S(n) = O(n)*/
+        //logic retrive from freq in reverse using 2 for loops
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) { //num and corr freq
+            freq[entry.getValue()].add(entry.getKey()); //go to array and its AL
+        }
+        int index = 0;
+        int[] res = new int[k];
+        for (int i = freq.length - 1; i >= 0 && index < k; i--) {
+            for (int n : freq[i]) {
+                res[index++] = n;
+                if (index == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
