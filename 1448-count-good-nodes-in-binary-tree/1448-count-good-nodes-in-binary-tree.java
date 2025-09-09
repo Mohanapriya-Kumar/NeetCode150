0b882a5b7733d5af61acd3bee698b9dd3f196733
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2025-09-09 09:44:56 +0530

    Time: 2 ms (83.76%), Space: 52.2 MB (88.2%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -15,20 +15,15 @@
  */
 class Solution {
     public int goodNodes(TreeNode root) {
-        /*Use DFS to compute maxSoFar
-        T(n) = O(n)
-        S(n) = O(h) = O(logn) or O(n)*/
-        return dfs(root,root.val);
+            return helper(root, Integer.MIN_VALUE);
     }
-    private int dfs(TreeNode node, int maxSoFar){
-        if(node == null) return 0;
-        int count = 0;
-        if(node.val >= maxSoFar){
-            count = 1;//Local for recursive call
-            maxSoFar = node.val;
-        }
-        count += dfs(node.left,maxSoFar); //recursion
-        count += dfs(node.right,maxSoFar);
-        return count;
+
+    private int helper(TreeNode root, int max){
+            if(root == null) return 0;
+            if(root.val >= max){
+                return 1 + helper(root.left, root.val) + helper(root.right, root.val);
+            }
+
+            return helper(root.left, max) + helper(root.right, max);
     }
 }
