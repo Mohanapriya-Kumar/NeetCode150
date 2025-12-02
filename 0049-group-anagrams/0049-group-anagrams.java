8dcb78f90bf55b0f0779d0730d18de558a35d5b1
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2025-12-02 23:54:48 +0530

    Time: 18 ms (24.79%), Space: 49.4 MB (45.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,10 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        /*Using HashMap to store key(pattern of 0's & 1's that can produce Ansgrams)
+        T(n) = O(m*n) m = no. of s in strs, n = length of longest s: 2 loops
+        S(n) = O(m) Hashet of no. of strs
+        */
+        //Logic: First find key of each s then put in map if absent then add s to the key(List)
         Map<String, List<String>> map = new HashMap<>();
         for(String s : strs){
             int[] freq = new int[26];
