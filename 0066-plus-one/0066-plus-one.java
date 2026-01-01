f0b03f7ee69a3fb618a9604a95446a1e194844ee
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2026-01-01 21:55:36 +0530

    Time: 0 ms (100%), Space: 43.6 MB (7.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
         for(int i = n-1; i >= 0; i--){
-            if(digits[i] < 9){
+            if(digits[i] < 9){ //check if +1/carry can be put on this digit directly
                 digits[i]++;
                 return digits;
             }
-            digits[i] = 0;
+            digits[i] = 0; //digit is 9, it bcms 0 and carry goes to left digit
         }
-        int[] res = new int[n+1];
-        res[0] = 1;
+        int res[] = new int[n+1]; //all digits are 9 need one more place
+        res[0] = 1; //other digits = 0
         return res;
     }
 }
