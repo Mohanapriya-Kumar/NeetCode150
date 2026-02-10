3c9298b805b6474c1c74d536e49b0eddc0dca963
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2026-02-10 22:51:16 +0530

    Time: 4 ms (80.06%), Space: 44.4 MB (74.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,14 @@
 class Solution {
-    /*Using simple ++ & --
-    T(n) = O(m+n) for(..m) for(..n)
-    S(n) = O(1) const arr[26]
-    Logic: ++ char freq for occurrrence in s then -- for corresponding t*/
     public boolean isAnagram(String s, String t) {
-        int[] freq = new int[26]; //for 26 alphabets
+        int freq[] = new int[26];
         for(int i = 0; i < s.length(); i++){
-            freq[s.charAt(i) - 'a']++; //-'a' to convert ascii to index from 0
+            freq[s.charAt(i) - 'a']++;
         }
         for(int i = 0; i < t.length(); i++){
             freq[t.charAt(i) - 'a']--;
         }
-        for(int i : freq){
-            if(i != 0) return false;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0) return false;
         }
         return true;
     }
