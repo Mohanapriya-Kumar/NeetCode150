11e8ad49c6e4857e00281b75924850497faf5f4a
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2026-02-10 22:46:41 +0530

    Time: 1 ms (60.8%), Space: 47.8 MB (56.86%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,14 +1,12 @@
 class Solution {
     public int trap(int[] height) {
-        /*Two ptr approach
-        T(n) = O(n)
-        S(n) = O(1)*/
-        //logic l < r => l++ res+=leftMax-height[l] simly for r
         int l = 0, r = height.length - 1;
         int leftMax = height[l], rightMax = height[r];
         int res = 0;
+        //if somewhere behind taller block exists => left can hold water
+        // |||y for right frwd
         while(l < r){
-            if(height[l] < height[r]){
+            if(height[l] < height[r]){ //if small check if capable of holdin H2O
                 l++;
                 leftMax = Math.max(leftMax,height[l]);
                 res += leftMax - height[l];
